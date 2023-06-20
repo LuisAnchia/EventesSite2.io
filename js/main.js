166449277b4f2dfbd5bd77e27bd4d17445b8e022
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-06-20 00:06:48 -0600

    up
    up

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ const eventCache = new Proxy({}, {
 });
 
 document.querySelector('.tab-nav').addEventListener('click', async function(event) {
-  if (event.target.tagName === 'A') {
+  if (event.target.classList.contains('category-button')) {
     const category = event.target.dataset.category;
     const events = await eventCache[category];
     renderEvents(events);

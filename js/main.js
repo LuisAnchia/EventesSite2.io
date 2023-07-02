db05e5ea7d9d3ea8a2f6cd83168aac139a942ccf
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-07-02 02:05:30 -0600

    up
    up

--- js/main.js
+++ js/main.js
@@ -1,22 +1,83 @@
-import { eventCache} from './services/proxy.js';
-import { renderEvents } from './utils/events.js';
-import { initializeButtonListeners } from './modules/buttonsChange.js';
-import { eventListeners } from './utils/buttonsTarget.js';
+const state = {
+  interested: {
+    handleClick: function (item, interestedButton, goingButton) {
+      interestedButton.style.display = 'none';
+      if (goingButton) {
+        goingButton.style.display = 'inline-block';
+      }
+      const message = document.createElement('p');
+      message.textContent = "You're interested in going";
+      message.classList.add('going-message');
+      item.insertBefore(message, goingButton);
+      const changedMindButton = document.createElement('button');
+      changedMindButton.textContent = "Changed your mind?";
+      changedMindButton.classList.add('changed-mind-button');
+      changedMindButton.addEventListener('click', function () {
+        item.removeChild(message);
+        item.removeChild(changedMindButton);
+        interestedButton.style.display = 'inline-block';
+      });
+      item.appendChild(changedMindButton);
+      return {
+        state: state.going,
+        message: message,
+        changedMindButton: changedMindButton,
+        button: interestedButton,
+      };
+    },
+  },
+  going: {
+    handleClick: function (item, goingButton, interestedButton) {
+      goingButton.style.display = 'none';
+      if (interestedButton) {
+        interestedButton.style.display = 'none';
+      }
+      const message = document.createElement('p');
+      message.textContent = "You're going to an event";
+      message.classList.add('going-message');
+      item.appendChild(message);
+      const changedMindButton = document.createElement('button');
+      changedMindButton.textContent = "Changed your mind?";
+      changedMindButton.classList.add('changed-mind-button');
+      changedMindButton.addEventListener('click', function () {
+        item.removeChild(message);
+        item.removeChild(changedMindButton);
+        goingButton.style.display = 'inline-block';
+        if (interestedButton) {
+          interestedButton.style.display = 'inline-block';
+        }
+      });
+      item.appendChild(changedMindButton);
+      return {
+        state: state.interested,
+        message: message,
+        changedMindButton: changedMindButton,
+        button: goingButton,
+      };
+    },
+  },
+};
 
-async function getAndRenderEvents(category) {
-  const events = await eventCache[category];
-  renderEvents(events);
-}
-window.addEventListener('load', async () => {
-  await getAndRenderEvents('music');
+let currentState = state.interested;
 
-  initializeButtonListeners();
-  eventListeners();
-});
-
-document.querySelector('.tab-nav').addEventListener('click', async function (event) {
-  if (event.target.classList.contains('category-button')) {
-    const category = event.target.dataset.category;
-    await getAndRenderEvents(category);
+function handleClick(event) {
+  if (event.target.classList.contains('interested-button')) {
+    const item = event.target.parentNode;
+    const interestedButton = event.target;
+    const goingButton = item.querySelector('.going-button');
+    const newState = currentState.handleClick(item, interestedButton, goingButton);
+    currentState = newState.state;
   }
-});
+  if (event.target.classList.contains('going-button')) {
+    const item = event.target.parentNode;
+    const goingButton = event.target;
+    const interestedButton = item.querySelector('.interested-button');
+    const newState = currentState.handleClick(item, goingButton, interestedButton);
+    currentState = newState.state;
+    interestedButton.style.display = 'none';
+  }
+}
+
+export const eventListeners = function () {
+  document.addEventListener('click', handleClick);
+}

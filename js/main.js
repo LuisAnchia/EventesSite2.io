2dbbe38faf9aebceb658c815478ab362ace68ba8
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-07-02 01:47:16 -0600

    upo
    upo

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { eventCache} from './services/Proxy.js';
+import { eventCache} from './services/proxy.js';
 import { renderEvents } from './utils/events.js';
 import { initializeButtonListeners } from './modules/buttonsChange.js';
 import { eventListeners } from './utils/buttonsTarget.js';

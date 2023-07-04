ced1b57d8c8581beca7724f59ba9b843e70e07b3
LuisAnchia <108684856+LuisAnchia@users.noreply.github.com>
2023-07-04 15:38:24 -0600

    up
    up

--- js/utils/events.js
+++ js/utils/events.js
@@ -15,6 +15,12 @@ export function renderEvents(events) {
       
       <button class="interested-button" data-event-id="${event.id}">Interested</button>
       <button class="going-button" data-event-id="${event.id}">Going!</button>
+      <button class="new-button" data-event-id="${event.id}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+          <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+          <path d="M19.5 13.572l-7.5 7.428l-7.5 -7.428m0 0a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572"/>
+        </svg>
+      </button>
     `;
     grid.appendChild(item);
   });
